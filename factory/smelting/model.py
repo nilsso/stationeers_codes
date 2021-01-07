355283da671578f33a15e000d7248c62fb382d52
Nils Olsson <nilso@enosis.net>
2021-01-06 18:58:10 -0800

    Lazy update

--- factory/smelting/model.py
+++ factory/smelting/model.py
@@ -10,29 +10,33 @@ vF = 1000
 
 P = lambda p, v, t: (p*v)/(t*R)
 
-tH = 2200
-nH = 100
-tC = 200
-nC = 100
+tH = 2300
+nH = 100000
+tC = 230
+nC = 100000
 
 pMin = 100
 pMax = 50000
 tMin = 200
 tMax = 2300
 
-tT0 = 1500
-pT0 = 10000
-tF0 = 800
-pF0 = 5000
+# tT0 = 1500
+# pT0 = 4000
+tT0 = 1889.00
+pT0 = 17948.00
+
+# tF0 = 800
+# pF0 = 3000
+tF0 = 2108.00
+pF0 = 45300.00
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-plt.subplots_adjust(left=0.25, bottom=0.25)
-plt.suptitle('''\
-$t_T n_T=t_F(n_F-n_R)+t_I n_I$
-''')
-ax.view_init(elev=15, azim=135)
-# ax.plot_surface(x, y, z, cmap=plt.cm.coolwarm)
+# plt.subplots_adjust(left=0.25, bottom=0.25)
+# plt.suptitle('''\
+# $t_T n_T=t_F(n_F-n_R)+t_I n_I$
+# ''')
+# ax.view_init(elev=15, azim=135)
 
 pFax = plt.axes([0.1, 0.05+0.03*3, 0.8, 0.02])
 tFax = plt.axes([0.1, 0.05+0.03*2, 0.8, 0.02])
@@ -44,31 +48,40 @@ tFslider = Slider(tFax, '$t_F$', tMin, tMax, valinit=tF0)
 pTslider = Slider(pTax, '$p_T$', pMin, pMax, valinit=pT0)
 tTslider = Slider(tTax, '$t_T$', tMin, tMax, valinit=tT0)
 
-n = 50 # mesh points per axis
+n = 1000 # mesh points per axis
 
 def update(_=None):
+    global nR
+    global nI
+    global tI
     tT, pT, tF, pF = tTslider.val, pTslider.val, tFslider.val, pFslider.val
     nT = P(pT, vF, tT)
     nF = P(pF, vF, tF)
-    a = np.linspace(0, nF, n) # nR in [0, nF]
-    b = np.linspace(10, nH+nC, n) # nI in [0, nH+nC]
+    nR = np.linspace(0, nF, n) # nR in [0, nF]
+    nI = np.linspace(10, nH+nC, n) # nI in [0, nH+nC]
     # x: nR moles removed
     # y: nI moles input
     # z: tI temp input
-    x, y = np.meshgrid(a, b)
-    fz = np.vectorize(lambda nR, nI: (tT*nT-tF*(nF-nR))/nI)
-    z = fz(x, y)
-    z = np.ma.masked_outside(fz(x, y), tMin, tMax)
+    nR, nI = np.meshgrid(nR, nI)
+    ftI = np.vectorize(lambda nR, nI: (tT*nT-tF*(nF-nR))/nI)
+    # tI = ftI(nR, nI)
+    tI = np.ma.masked_outside(ftI(nR, nI), tMin, tMax)
     ax.clear()
-    ax.scatter(x, y, z, cmap=plt.cm.coolwarm, vmin=tMin, vmax=tMax)
+    # ax.plot_surface(nR, nI, tI)
+    ax.scatter([1809.22], [367.37], [1426.77], color='red')
+    ax.scatter(nR, nI, tI, alpha=.1)
     ax.set_xlabel('$n_R$ moles removed')
     ax.set_ylabel('$n_I$ moles input')
     ax.set_zlabel('$t_I$ temperature input')
+    ax.set_xlim3d(0, np.max(nR))
     ax.set_zlim3d(tMin, tMax)
+
 
 for s in [tTslider, pTslider, tFslider, pFslider]:
     s.on_changed(update)
 
 update()
+
+print(np.nanmin(np.array([nR, nI, tI]), axis=(1,2), keepdims=True))
 
 plt.show()
--- factory/smelting/simulation.py
+++ factory/smelting/simulation.py
@@ -3,51 +3,89 @@ from random import random
 R = 8.31446261815324
 cp = 28.2 # CO2
 
-# pv=nRt
-
-def P(v, n, t):
-    return (n*R*t)/v
-
-def V(p, n, t):
-    return (n*R*t)/p
-
-def N(p, v, t):
+def moles(p, v, t):
     return (p*v)/(R*t)
-
-def T(p, v, n):
-    return (p*v)/(n*R)
-
-def ta(na, tb, nb, tc, nc):
-    return (tc*nc-tb*nb)/na
 
 vF = 1000
 vI = 1500
 vH = 500
 vC = 500
 
-tH = 100
-nH = 100
+tH = 2300 # hot source temperature
+tC = 230 # cold source temperature
 
-tC = 1
-nC = 100
+# tT = 1000
+# pT = 10000
+tT = tC+random()*(tH-tC)
+pT = random()*60000
+# tT = 980.17
+# pT = 6389.91
 
-tF = 10
-nF = 12
+# tF = 500
+# pF = 800
+tF = tC+random()*(tH-tC)
+pF = random()*60000
+# tF = 978.15
+# pF = 55593.88
 
-tT = 10
-nT = 10
+class Params:
+    def __init__(self, t, p, v):
+        self.t = t
+        self.p = p
+        self.v = v
 
-nD = 0
+    def unpack(self):
+        return self.t, moles(self.p, self.v, self.t)
 
-# print(nF)
-print(nF-tT*nT/tF)
+def findSolution(Tparams, Fparams, tH, tC, nRs):
+    tT, nT = Tparams.unpack()
+    tF, nF = Fparams.unpack()
+    nR = 0
+    nI = nT-nF+nR
+    tI = (tT*nT-tF*(nF-nR))/nI
+    nH = nI*(tI-tC)/(tH-tC)
+    nC = nI-nH
+    while nH < 0 or nC < 0 and nR <= nF:
+        nR += nRs
+        nI = nT-nF+nR
+        tI = (tT*nT-tF*(nF-nR))/nI
+        nH = (tI*nI-tC*nI)/(tH-tC)
+        nC = nI-nH
+    return tF, nF, nR, nH, nC
 
-if nF >= nT:
-    nF = tT*nT/tF
+print(f'''\
+SIMULATION
 
-# print(nF)
+tT (target temp)     = {tT:.2f}
+pT (target pressure) = {pT:.2f}
+nT (target moles)    = {moles(pT, vF, tT):.2f}
 
-# nI = nT-nF
-# tI = (tT*nT-tF*nF)/nI
-# nH = nI*(tI-tC)/(tH-tC)
-# nC = nI-nH
+tF (furnace initial temp)     = {tF:.2f}
+pF (furnace initial pressure) = {pF:.2f}
+nF (furnace initial moles)    = {moles(pF, vF, tF):.2f}''')
+
+nRs = 10
+res = findSolution(Params(tT, pT, vF), Params(tF, pF, vF), tH, tC, nRs)
+
+if res:
+    tF, nF, nR, nH, nC = res
+    nF -= nR
+    nI = nH+nC
+    tI = (tH*nH+tC*nC)/nI
+    tF = (tF*nF+tI*nI)/(nF+nI)
+    nF += nI
+    pF = nF*R*tF/vF
+    print(f'''
+- (stepping up nR in {nRs:.2f} increments)
+- Removing nR = {nR:.2f} moles
+- Added nH = {nH:.2f} moles from H to I
+- Added nC = {nC:.2f} moles from C to I
+- I has temperature tI = {tI:.2f}
+              moles nI = {nI:.2f}
+- Added I to F
+- Result temperature tF = {tF:.2f}
+- Result pressure    pF = {pF:.2f}
+''')
+else:
+    print('No solution')
+
